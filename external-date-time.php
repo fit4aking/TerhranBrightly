b87f44c077d140b39a7db21ced2c850b6937d4be
fit4aking <46458053+fit4aking@users.noreply.github.com>
2019-06-27 11:24:46 +0100

    Add files via upload

---
+++ external-date-time.php
@@ -0,0 +1,272 @@
+<br /><form id="form1" name="form1" method="post" action="events-tickets-national-international.php">
+<!--        <table width="96%" border="0" cellspacing="4" cellpadding="2" summary=
+          "International events locator" class="placetable" bgcolor="#0000CC" align="center">-->
+          <table width="80%" border="0" cellspacing="3" align="center" class="table4">
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="content_text_edt">Date </td>
+          </tr>
+          <tr>
+            <td>
+            <!--<input name="eventDate" type="text" id="inputField" value="" class="with-default" />-->
+            <!--<input type="text">-->
+            
+            
+            
+<!--  <input id="input_01" class="datepicker" name="eventDate" type="text" autofocuss value="" data-valuee="2014-08-08" style="width: 100%; height: 70px; color: #333333; font-family: Arial, Helvetica, sans-serif; font-size: 130%; background-color: #CCC;" />--> 
+  
+  <input type="Text" name="eventDate" data-field="datetime" id="demo1"  size="25" class="cp_forms" />
+<div id="dtBox"></div>
+  
+  
+
+            
+            </td>
+          </tr>
+            <tr>
+              <td class="content_text_edt">Categories<br />
+              <select name="cat" id="cat" class="select_menu">
+            <option value="">Not Selected</option>
+            <option value="Accessories">Accessories</option>
+            <option value="Accounting">Accounting</option>
+            <option value="Acting">Acting</option>
+            <option value="Adult content">Adult content</option>
+            <option value="Advertising">Advertising</option>
+            <option value="Aging | Anti aging">Aging | Anti aging</option>
+            <option value="Air Travel">Air Travel</option>
+            <option value="Alcohol &amp; Tobacco">Alcohol &amp; Tobacco</option>
+            <option value="Ancestry">Ancestry</option>
+            <option value="Animal Related">Animal Related</option>
+            <option value="Apartments / Condos">Apartments / Condos</option>
+            <option value="Appliances">Appliances</option>
+            <option value="Architecture">Architecture</option>
+            <option value="Art">Art</option>
+            <option value="Auctions">Auctions</option>
+            <option value="Autos New">Autos New</option>
+            <option value="Autos Used">Autos Used</option>
+            <option value="Banking">Banking</option>
+            <option value="Beer &amp; Wine">Beer &amp; Wine</option>
+            <option value="Biking">Biking</option>
+            <option value="Birth Days">Birth Days</option>
+            <option value="Boating">Boating</option>
+            <option value="Books | Authors">Books | Authors</option>
+            <option value="Business">Business</option>
+            <option value="Cats">Cats</option>
+            <option value="Celebrities">Celebrities</option>
+            <option value="Charity">Charity</option>
+            <option value="Child related">Child related</option>
+            <option value="Chiropractor">Chiropractor</option>
+            <option value="Cleaning">Cleaning</option>
+            <option value="Coffee &amp; Tea">Coffee &amp; Tea</option>
+            <option value="Communication">Communication</option>
+            <option value="Conspiracies">Conspiracies</option>
+            <option value="Construction">Construction</option>
+            <option value="Cooking">Cooking</option>
+            <option value="Cooking">Cosmetics</option>
+            <option value="Counseling">Counseling</option>
+            <option value="Credit | Loan">Credit | Loan</option>
+            <option value="Currency Trading">Currency Trading</option>
+            <option value="Dating">Dating</option>
+            <option value="Dental">Dental</option>
+            <option value="Dogs">Dogs</option>
+            <option value="Drugs | Medicine">Drugs | Medicine</option>
+            <option value="Economy">Economy</option>
+            <option value="Education">Education</option>
+            <option value="Electronics">Electronics</option>
+            <option value="Email &amp; messaging">Email &amp; messaging</option>
+            <option value="Energy drinks">Energy drinks</option>
+            <option value="Energy Nuclear">Energy Nuclear</option>
+            <option value="Energy Oil &amp; Gas">Energy Oil &amp; Gas</option>
+            <option value="Energy Renewable">Energy Renewable</option>
+            <option value="Engineering">Engineering</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Environment">Environment</option>
+            <option value="Escort Service">Escort Service</option>
+            <option value="Events | Networking" Selected>Events | Networking</option>
+            <option value="Expats">Expats</option>
+            <option value="Family &amp; Marriage">Family &amp; Marriage</option>
+            <option value="Farming">Farming</option>
+            <option value="Fashion">Fashion</option>
+            <option value="Finance">Finance</option>
+            <option value="Fishing">Fishing</option>
+            <option value="Fitness &amp; Nutrition">Fitness &amp; Nutrition</option>
+            <option value="Flying">Flying</option>
+            <option value="Food">Food</option>
+            <option value="Football / Soccer">Football / Soccer</option>
+            <option value="Funding">Funding</option>
+            <option value="Furniture">Furniture</option>
+            <option value="Future">Future</option>
+            <option value="Gambling">Gambling</option>
+            <option value="Games">Games</option>
+            <option value="Gardening">Gardening</option>
+            <option value="Gifts">Gifts</option>
+            <option value="Golf">Golf</option>
+            <option value="Government">Government</option>
+            <option value="Graphics Design">Graphics Design</option>
+            <option value="Green Technology">Green Technology</option>
+            <option value="Guns &amp; Knifes">Guns &amp; Knifes</option>
+            <option value="Hair &amp; Make Up">Hair &amp; Make Up</option>
+            <option value="Health">Health</option>
+            <option value="Heating &amp; Cooling">Heating &amp; Cooling</option>
+            <option value="History">History</option>
+            <option value="Hobbies">Hobbies</option>
+            <option value="Home business">Home business</option>
+            <option value="Home Improvement">Home Improvement</option>
+            <option value="Horoscope | Psychic">Horoscope | Psychic</option>
+            <option value="Horse Related">Horse Related</option>
+            <option value="Hotels">Hotels</option>
+            <option value="Hunting">Hunting</option>
+            <option value="Immigration">Immigration</option>
+            <option value="Insurance">Insurance</option>
+            <option value="Intellectual Property">Intellectual Property</option>
+            <option value="Interior Design">Interior Design</option>
+            <option value="Internet">Internet</option>
+            <option value="Jewelry">Jewelry</option>
+            <option value="Jobs">Jobs</option>
+            <option value="Kitchen &amp; Bath">Kitchen &amp; Bath</option>
+            <option value="Language Learning">Language Learning</option>
+            <option value="Language Translation">Language Translation</option>
+            <option value="Law | Corporate">Law | Corporate</option>
+            <option value="Law | International">Law | International</option>
+            <option value="Law | Personal">Law | Personal</option>
+            <option value="LGBT">LGBT</option>
+            <option value="Linux Admin">Linux Admin</option>
+            <option value="Love &amp; Romance">Love &amp; Romance</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Martial Arts">Martial Arts</option>
+            <option value="Math">Math</option>
+            <option value="Media">Media</option>
+            <option value="Metals">Metals</option>
+            <option value="Military">Military</option>
+            <option value="Mining | Resources">Mining | Resources</option>
+            <option value="Mobile Phone">Mobile Phone</option>
+            <option value="Money">Money</option>
+            <option value="Motherhood">Motherhood</option>
+            <option value="Motorcycles">Motorcycles</option>
+            <option value="Movies">Movies</option>
+            <option value="Moving">Moving</option>
+            <option value="Music">Music</option>
+            <option value="Nature">Nature</option>
+            <option value="Networking">Networking</option>
+            <option value="News Related">News Related</option>
+            <option value="Night Life/Clubs">Night Life/Clubs</option>
+            <option value="Olympics">Olympics</option>
+            <option value="Other">Other</option>
+            <option value="PCs | Apple Computer">PCs | Apple Computer</option>
+            <option value="PCs | MS based">PCs | MS based</option>
+            <option value="Peace &amp; Good Will">Peace &amp; Good Will</option>
+            <option value="Photography">Photography</option>
+            <option value="Pizza &amp; Pasta">Pizza &amp; Pasta</option>
+            <option value="Politics Asia">Politics Asia</option>
+            <option value="Politics Europe">Politics Europe</option>
+            <option value="Politics USA">Politics USA</option>
+            <option value="Politics World">Politics World</option>
+            <option value="Psychology">Psychology</option>
+            <option value="Real Estate">Real Estate</option>
+            <option value="Relax Meditate">Relax Meditate</option>
+            <option value="Religion Buddhism">Religion Buddhism</option>
+            <option value="Religion Christian">Religion Christian</option>
+            <option value="Religion Islam">Religion Islam</option>
+            <option value="Religion Judaism">Religion Judaism</option>
+            <option value="Religion Others">Religion Others</option>
+            <option value="Restaurants">Restaurants</option>
+            <option value="Retirement">Retirement</option>
+            <option value="Rich | Wealthy">Rich | Wealthy</option>
+            <option value="Running">Running</option>
+            <option value="Sales">Sales</option>
+            <option value="Scams &amp; Frauds">Scams &amp; Frauds</option>
+            <option value="Science">Science</option>
+            <option value="Scuba Diving">Scuba Diving</option>
+            <option value="Sea">Sea</option>
+            <option value="Search Engine">Search Engine</option>
+            <option value="Security | Police">Security | Police</option>
+            <option value="Seminars">Seminars</option>
+            <option value="Senior Citizens">Senior Citizens</option>
+            <option value="Sexual in nature">Sexual in nature</option>
+            <option value="Shoes">Shoes</option>
+            <option value="Skill Training">Skill Training</option>
+            <option value="Skin Care">Skin Care</option>
+            <option value="Sleep | Dream">Sleep | Dream</option>
+            <option value="Social Network">Social Network</option>
+            <option value="Social Welfare">Social Welfare</option>
+            <option value="Software Development">Software Development</option>
+            <option value="Solar">Solar</option>
+            <option value="Spa | Massage">Spa | Massage</option>
+            <option value="Space">Space</option>
+            <option value="Sports">Sports</option>
+            <option value="SQL | MySQL">SQL | MySQL</option>
+            <option value="Stamps">Stamps</option>
+            <option value="Stocks &amp; Bonds">Stocks &amp; Bonds</option>
+            <option value="Studying">Studying</option>
+            <option value="Style">Style</option>
+            <option value="Supplies | Camping">Supplies | Camping</option>
+            <option value="Supplies | Cooking">Supplies | Cooking</option>
+            <option value="Supplies | Office">Supplies | Office</option>
+            <option value="Taxes">Taxes</option>
+            <option value="Teaching">Teaching</option>
+            <option value="Teens">Teens</option>
+            <option value="Tennis">Tennis</option>
+            <option value="Toys - Hobbies">Toys - Hobbies</option>
+            <option value="Transportation">Transportation</option>
+            <option value="Travel">Travel</option>
+            <option value="TV">TV</option>
+            <option value="UFOs">UFOs</option>
+            <option value="University">University</option>
+            <option value="Vacation">Vacation</option>
+            <option value="Vegetarian | Vegan">Vegetarian | Vegan</option>
+            <option value="Venture Capital">Venture Capital</option>
+            <option value="War &amp; Peace">War &amp; Peace</option>
+            <option value="Web development">Web development</option>
+            <option value="Web site hosting">Web site hosting</option>
+            <option value="Web site promotion">Web site promotion</option>
+            <option value="Weddings">Weddings</option>
+            <option value="Weight Loss">Weight Loss</option>
+            <option value="Women Issues">Women Issues</option>
+            <option value="Work | Employment">Work | Employment</option>
+            </select>
+          </td>   
+          </tr>
+              <tr>
+              <td class="content_text_edt">Region<br />
+              <select name="myregion" size="1" id="region" onchange=
+              "set_country(this,country,city_state)" style="width: 100%;
+	height:70px;
+	color: #333333;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 130%;">
+            
+            <script type="text/javascript">
+//<![CDATA[
+                setRegions(this);
+                //]]>
+                </script>
+          </select>
+            </td>
+            </tr>
+          <tr>
+            <td class="content_text_edt">Country <br />
+            <select name="mycountry" size="1" disabled="disabled"
+              id="country" onChange="set_city_state(this,city_state)" style="width: 100%;
+	height:70px;
+	color: #333333;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 130%;">
+              </select></td>
+          </tr>
+          <tr>
+            <td class="content_text_edt">City/State <br /><select name="mycitystate" size="1" disabled="disabled" id="city_state"
+              onchange="print_city_state(country,this)" style="width: 100%;
+	height:70px;
+	color: #333333;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 130%;">
+              </select></td>
+          </tr>
+          <tr>
+            <td><br /><input name="Submit" type="submit" class="pimpmybutton2" value="Submit" style="height:10%; width:100%; font-size:150%" /></td>
+          </tr>
+        </table>
+        <div id="container"></div>
+      </form><br /> 
