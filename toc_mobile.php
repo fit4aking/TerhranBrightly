8fc92dab69d2383d2e2a2383da788cc79b5c6406
fit4aking <46458053+fit4aking@users.noreply.github.com>
2019-06-27 11:22:38 +0100

    Add files via upload

---
+++ toc_mobile.php
@@ -0,0 +1,24 @@
+ <br />
+ <ul>
+  <li><a href="index.php">Home</a><br/><br/>
+    <li><a href="about-events-on-now.php">About</a> <br/><br/>
+    <li><a href="events-planning-guidance-kent-uk.php">Services</a> <br/><br/>
+    <li><a href="events-planning-faqs.php">FAQs</a> <br/><br/>
+    <li><a href="events-on-now-contact-details.php">Contact us</a> <br/><br/>
+    <li><a href="events-on-now-terms.php">Terms</a><br/><br/>
+</ul>
+<br />
+ <ul>   
+    <li><a href="register-account.php">Post Events</a><br/><br/>  
+    <li><a href="register-account-business-ads.php">Post a Free Business Directory Listing</a><br/><br/> 
+    <li><a href="skill-exchange-registration.php">Post a Skill Exchange</a><br/><br/>
+  </ul>
+  <br />
+  <ul>  
+    <li><a href="../../xmchat/index.php">Login to Chat</a><br/><br/>
+    <li><a href="crypt/index.php">Login to Control Panel</a><br/><br/>
+    <li><a href="business-advert-categories.php">Search Directory Categories</a><br/><br/>   
+    <li><a href="skill-exchange-categories.php">Search Skill Exchanges</a><br/><br/> 
+    <li><a href="it-solutions.php">IT Solutions</a><br/><br/>
+</ul>
+
